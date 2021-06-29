a5defbd6a3c57faf872fb868ac38f47bae706ece
Nicholas Maheshwari <nicholasmaheshwari@gmail.com>
2021-06-29 11:54:45 -0700

    created question model

---
+++ app/src/main/java/com/ebookfrenzy/androidquiz/Question.java
@@ -0,0 +1,41 @@
+package com.ebookfrenzy.androidquiz;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question {
+
+    private String english;
+    private String latin;
+    private String greek;
+
+    private Set<String> wrongAnswers = new HashSet<String>();
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public String getLatin() {
+        return latin;
+    }
+
+    public String getGreek() {
+        return greek;
+    }
+
+    public Set<String> getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public void addWrongAnswer(String wrong){
+        wrongAnswers.add(wrong);
+    }
+
+    public String getLatinQuestionText(){
+        return "What is the Latin of " + english + "?";
+    }
+
+    public String getGreekQuestionText(){
+        return "What is the Greek of " + english + "?";
+    }
+}
